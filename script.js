b634fcb4fe88dd3cfa0080d837820fd0aa001feb
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2024-06-08 14:45:01 -0300

    atualizacao

---
+++ script.js
@@ -0,0 +1,9 @@
+var msg2 = document.querySelector('#msg2');
+var icone2 = document.querySelector('#icone2');
+msg2.innerHTML = 'olá mundo';
+
+let carro = "bi bi-truck"
+let cone = "bi bi-cone-striped"
+let chekok = "bi bi-check2-circle"
+
+icone2.classList = carro;
