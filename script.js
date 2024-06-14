c8cb8164fe22a3cb6b7ff4814dcadb90174588b9
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-06-14 10:41:52 -0300

    atualizacao

--- script.js
+++ script.js
@@ -3,26 +3,11 @@
 
 
 
-    
-function salvar(){
-    
-    let sqlite3 = require('sqlite3').verbose();
-    alert('dados salvo')
-    let db = new sqlite3.Database('bancokanban.db');
-    let check;
-    db.serialize(function(){
-        db.prepare("INSERT INTO carregamento (CLT,MOT,DEST,CONF,PLACA,CUB) VALUES ('T', 'JOÃO DE DEUS DA LUZ', 'MARANHÃO / PARÁ / TOCANTINS',  'FRANCISCO', 'KBI-6155', '80')")
-        
-    });
-        
-    db.close();
-}
+
 
     
 
 
-
-    
 
 let Dados = ['T', 'JOÃO DE DEUS DA LUZ', 'MARANHÃO / PARÁ / TOCANTINS',  'FRANCISCO', 'KBI-6155', '80'];
 
